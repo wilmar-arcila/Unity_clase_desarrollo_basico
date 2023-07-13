fc15e9518c467fdaa9f51522fcf29719a2c4bc19
wilmar-arcila <wilmararcilac@gmail.com>
2023-07-13 18:18:50 -0500

    Lógica del wall slide/jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -15,10 +15,15 @@ public class CharacterController : MonoBehaviour
     private Animator animator;      // Variable para mantener la referencia al componente Animator
     private SpriteRenderer spriteR; // Variable para mantener la referencia al componente SpriteRenderer
 
-    bool enElPiso = false; // Bandera que verifica que el personaje ha tocado el piso
-    bool hasJump  = false; // Bandera que indica que el personaje ha realizado el primer salto
+    private RaycastHit2D HitL, HitR;
+
+    bool enElPiso  = false; // Bandera que verifica que el personaje ha tocado el piso
+    bool enElMuroL = false; // Bandera que verifica que el personaje ha tocado el muro izquierdo
+    bool enElMuroR = false; // Bandera que verifica que el personaje ha tocado el muro derecho
+    bool hasJump   = false; // Bandera que indica que el personaje ha realizado el primer salto
 
     [SerializeField] private AudioSource salto_SFX;
+    [SerializeField] private LayerMask rayMask;
     
     void Start()
     {
@@ -28,10 +33,18 @@ public class CharacterController : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();       // Se obtiene la referencia al componente Rigidbody2D del personaje
         animator = GetComponent<Animator>();      // Se obtiene la referencia al componente Animator del personaje
         spriteR = GetComponent<SpriteRenderer>(); // Se obtiene la referencia al componente SpriteRenderer del personaje
+        
     }
 
     void Update()
     {
+        // Se dibujan los rayos solo para DEPURACIÓN
+        Debug.DrawRay(transform.position, 0.6f*transform.right, Color.red);
+        Debug.DrawRay(transform.position, -0.6f*transform.right, Color.red);
+
+        HitR = Physics2D.Raycast(transform.position, transform.right, 0.6f, rayMask);
+        HitL = Physics2D.Raycast(transform.position, transform.right, -0.6f, rayMask);
+
         // Si el personaje está rotando mucho se vuelve a poner vertical para evitar
         // que se vaya a quedar acostado en el piso
         if(transform.rotation.z > 0.3 || transform.rotation.z < -0.3){
@@ -94,6 +107,45 @@ public class CharacterController : MonoBehaviour
             enElPiso = false;
         }
 
+        // Implementación del salto del muro
+        if(Input.GetKeyDown("space") && (enElMuroL || enElMuroR)){
+            Debug.Log("WALL JUMP");
+            animator.SetBool("jump", true);
+            if(enElMuroL){
+                rb2d.AddForce(new Vector2(fuerzaImpulso, -0.5f*fuerzaSalto*Physics2D.gravity[1]*rb2d.mass));
+                enElMuroL = false;
+            }
+            else{
+                rb2d.AddForce(new Vector2(-fuerzaImpulso, -0.5f*fuerzaSalto*Physics2D.gravity[1]*rb2d.mass));
+                enElMuroR = false;
+            }            
+        }
+
+        // Personaje tocando un muro
+        if(HitL.collider != null){ // izquierdo
+            Debug.Log("WALL LEFT");
+            rb2d.gravityScale = 0.1f;
+            animator.SetBool("wall", true);
+            enElMuroL = true;
+            enElPiso = false;
+        }
+        else if(HitR.collider != null){ //derecho
+            Debug.Log("WALL RIGHT");
+            rb2d.gravityScale = 0.1f;
+            animator.SetBool("wall", true);
+            enElMuroR = true;
+            enElPiso = false;
+        }
+
+        // Personaje en el aire
+        if((HitL.collider == null) && (HitR.collider == null) && !enElPiso){
+            Debug.Log("AIRE");
+            animator.SetBool("wall", false);
+            enElMuroL = false;
+            enElMuroR = false;
+            rb2d.gravityScale = 1f;
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -108,13 +160,9 @@ public class CharacterController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        Debug.Log("Caída");
-        vidas -= 1;
-        Debug.Log("VIDAS: " + vidas);
-        if(vidas <= 0){
-            Debug.Log("GAME OVER");
-            vidas = 3;
+    private void OnTriggerEnter2D(Collider2D collider){
+        if(collider.tag == "FallDetector"){
+            Debug.Log("Caída");
         }
         gameObject.transform.position = new Vector3(-1.92f,nivelTecho,0);
     }
