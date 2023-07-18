b99ab750133525414097cf3d3f9a52aeac062895
wilmar-arcila <wilmararcilac@gmail.com>
2023-07-18 12:31:03 -0500

    Actualización de prefabs, gitignore y README

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,6 +7,22 @@ public class SceneController : MonoBehaviour
 {
     public void changeScene(int _escena){
       Debug.Log("CALL -> changeScene: " + _escena);
+      AudioSource audio = gameObject.GetComponent<AudioSource>(); //Si el objeto no tiene componente de audio esta variable será NULL
+      
+      if(audio == null){   // Si el objeto NO TIENE componente de AUDIO se cambia de escena inmediatamente
+         SceneManager.LoadScene(_escena);
+      }
+      else{                // Si el objeto SI TIENE componente de AUDIO solo se cambia de escena cuando el audio haya terminado de reproducirse
+         Debug.Log("[changeScene] AudioSource:  " + audio);
+         StartCoroutine(changeSceneWithAudio(_escena, audio));
+      }
+      Debug.Log("CALL -> changeScene: " + _escena + " END");
+   }
+
+   private IEnumerator changeSceneWithAudio(int _escena, AudioSource audio){
+      Debug.Log("[changeScene]Yield Init");
+      yield return new WaitUntil(() => audio.isPlaying == false); // Se ejecuta esta línea hasta que la condición sea verdadera
+      Debug.Log("[changeScene]Yield Over");
       SceneManager.LoadScene(_escena);
    }
 
@@ -16,4 +32,5 @@ public class SceneController : MonoBehaviour
       #endif
       Application.Quit();
    }
+
 }
