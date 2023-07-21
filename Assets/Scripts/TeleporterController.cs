ef868c56bf422e9bc32fddc034bb3b920dca2107
wilmar-arcila <wilmararcilac@gmail.com>
2023-07-20 20:33:28 -0500

    Inclusión de Copa Trasladora

---
+++ Assets/Scripts/TeleporterController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleporterController : MonoBehaviour
+{
+    [SerializeField] private AudioSource teleporter_SFX;
+    [SerializeField] private int destinationEscene;
+
+    private SceneController sceneController;
+    private Animator animator;
+
+    private void Start() {
+        sceneController = transform.GetComponent<SceneController>();
+        animator = transform.GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider){
+        Debug.Log("Teleporter");
+        teleporter_SFX.Play();
+        animator.SetTrigger("collected");
+
+        // ESPERAR A QUE TERMINE LA REPRODUCCIÓN DEL SONIDO
+        sceneController.changeScene(destinationEscene);       
+    }
+}
