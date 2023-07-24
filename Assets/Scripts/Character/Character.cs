3c0a14d3d534ddb32f151f7a3bf484a4ba561bba
wilmar-arcila <wilmararcilac@gmail.com>
2023-07-24 13:16:47 -0500

    Manejo de datos persistentes -ScriptableObject Character: almacena nombre, imagen y prefab. -GameManager: Singleton para mantener el listado de los personajes. Como su nombre lo indica, puede ser utilizado de forma mas general.

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NuevoPersonaje", menuName = "Playables/Personaje", order = 1)]
+
+public class Character : ScriptableObject
+{
+    public GameObject personaje;
+
+    public Sprite imagenPersonaje;
+
+    public string nombrePersonaje;    
+}
